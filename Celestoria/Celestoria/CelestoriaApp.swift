38d29a8fd4b6d192ff652aac7f09829429651f63
Me1e <mele0404@naver.com>
2025-01-16 14:18:17 +0900

    init

---
+++ Celestoria/Celestoria/CelestoriaApp.swift
@@ -0,0 +1,33 @@
+//
+//  CelestoriaApp.swift
+//  Celestoria
+//
+//  Created by Minjun Kim on 1/16/25.
+//
+
+import SwiftUI
+
+@main
+struct CelestoriaApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
