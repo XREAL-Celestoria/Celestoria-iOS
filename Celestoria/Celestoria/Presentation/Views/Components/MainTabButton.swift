384ff27c7cfdd8c17e3d8e56617e511678de71c1
Me1e <mele0404@naver.com>
2025-01-21 14:20:57 +0900

    style: MainTabButton 디자인 반영

--- Celestoria/Celestoria/Presentation/Views/Components/MainTabButton.swift
+++ Celestoria/Celestoria/Presentation/Views/Components/MainTabButton.swift
@@ -18,22 +18,39 @@ struct MainTabButton: View {
                 VStack {
                     ZStack {
                         Circle()
-                            .fill(Color.NebulaBlack)
-                            .overlay(
-                                Circle()
-                                    .fill(LinearGradient.MainTabButtonBackground.opacity(0.3))
+                            .fill(
+                                LinearGradient(
+                                    stops: [
+                                        .init(color: .white.opacity(0.5), location: 0.00),
+                                        .init(color: Color(red: 0.92, green: 0.92, blue: 0.92).opacity(0.37), location: 0.20),
+                                        .init(color: Color(red: 0.88, green: 0.88, blue: 0.88).opacity(0.29), location: 0.28),
+                                        .init(color: Color(red: 0.83, green: 0.83, blue: 0.83).opacity(0.21), location: 0.40),
+                                        .init(color: Color(red: 0.81, green: 0.81, blue: 0.81).opacity(0.18), location: 0.48),
+                                        .init(color: Color(red: 0.79, green: 0.79, blue: 0.79).opacity(0.14), location: 0.54),
+                                        .init(color: Color(red: 0.78, green: 0.78, blue: 0.78).opacity(0.13), location: 0.59),
+                                        .init(color: Color(red: 0.77, green: 0.77, blue: 0.77).opacity(0.1), location: 0.67),
+                                    ],
+                                    startPoint: UnitPoint(x: -0.21, y: -0.17),
+                                    endPoint: UnitPoint(x: 1, y: 1)
+                                )
                             )
-                            .shadow(color: Color.black.opacity(0.18), radius: 50, x: 0, y: 4)
+                            .shadow(color: .black.opacity(0.18), radius: 49.92025, x: 0, y: 4.16002)
                             .overlay(
                                 Circle()
+                                    .inset(by: 1.34)
                                     .stroke(
-                                        LinearGradient.MainTabButtonStroke,
+                                        LinearGradient(
+                                            stops: [
+                                                .init(color: .white.opacity(0.2), location: 0.1414),
+                                                .init(color: .clear, location: 0.4767),
+                                                .init(color: .white.opacity(0.2), location: 0.8263)
+                                            ],
+                                            startPoint: UnitPoint(x: 0, y: 0),
+                                            endPoint: UnitPoint(x: 1, y: 1)
+                                        ),
                                         lineWidth: 2.69
                                     )
                             )
-                        Circle()
-                            .fill(.ultraThinMaterial)
-                            .opacity(0.2)
                         
                         Image(systemName: imageName)
                             .resizable()
@@ -42,8 +59,10 @@ struct MainTabButton: View {
                             .foregroundColor(.NebulaWhite)
                     }
                     .frame(width: geometry.size.width, height: geometry.size.width)
+                    
                     Spacer()
                         .frame(height: geometry.size.height * 1)  
+                    
                     Text(text)
                         .font(.system(size: 19, weight: .bold))
                         .foregroundColor(.NebulaWhite)
