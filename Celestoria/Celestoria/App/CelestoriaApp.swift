e9c229253ed1a63a25c11d4ce697d457eb33c8b5
Me1e <mele0404@naver.com>
2025-01-27 18:05:53 +0900

    Feat: Set Fixed Window Size and Resizability for App Views
    - Add fixed window size (1280x720) for ContentView, AddMemoryContentView, and MemoryDetailView
    - Implement `.windowResizability(.contentSize)` for main, add memory, and memory detail windows

--- Celestoria/Celestoria/App/CelestoriaApp.swift
+++ Celestoria/Celestoria/App/CelestoriaApp.swift
@@ -16,41 +16,43 @@ struct CelestoriaApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .frame(width: 1280, height: 720)
                 .environmentObject(diContainer.appModel)
                 .environmentObject(diContainer.mainViewModel)
                 .environmentObject(diContainer.loginViewModel)
                 .environmentObject(diContainer.settingViewModel)
                 .environmentObject(diContainer.galaxyViewModel)
         }
+        .windowResizability(.contentSize)
         
         ImmersiveSpace(id: diContainer.appModel.immersiveSpaceID) {
             SpaceImmersiveView()
                 .environmentObject(diContainer.spaceCoordinator)
         }
         
-        
         WindowGroup("Add Memory", id: "Add-Memory") {
             if diContainer.appModel.showAddMemoryView {
                 AddMemoryContentView()
+                    .frame(width: 1280, height: 720)
                     .environmentObject(diContainer.appModel)
                     .environmentObject(diContainer.addMemoryMainViewModel)
                     .environmentObject(diContainer.mainViewModel)
                     .environmentObject(diContainer.spaceCoordinator)
             }
         }
+        .windowResizability(.contentSize)
         
         WindowGroup(id: "Memory-Detail", for: Memory.self) { $memory in
-            // - Swift 5.7~5.8 이후에는 `$memory.wrappedValue` 형태 사용.
-            // - `$memory`가 Binding<Memory?> 형태이므로 옵셔널 처리
             if let unwrappedMemory = $memory.wrappedValue {
                 MemoryDetailView(memory: unwrappedMemory)
+                    .frame(width: 1280, height: 720)
                     .environmentObject(diContainer.appModel)
             } else {
-                // fallback UI
                 Text("No memory provided.")
+                    .frame(width: 1280, height: 720)
             }
         }
-
+        .windowResizability(.contentSize)
     }
 }
 
