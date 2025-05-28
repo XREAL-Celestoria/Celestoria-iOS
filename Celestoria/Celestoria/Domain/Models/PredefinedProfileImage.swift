947acb9474b80b5787a1d58e758383d6153900af
30 <1224tpdud@gmail.com>
2025-05-28 14:11:05 +0900

    ✨ Profile random 설정

--- Celestoria/Celestoria/Domain/Models/PredefinedProfileImage.swift
+++ Celestoria/Celestoria/Domain/Models/PredefinedProfileImage.swift
@@ -10,6 +10,11 @@ import SwiftUI
 
 enum PredefinedProfileImage: String, CaseIterable, Equatable {
     case profile_gray, profile_blue, profile_pink, profile_purple, profile_green, profile_yellow, profile_blue_green, profile_orange
+    
+    // 랜덤하게 Starfield enum 값을 반환
+    static func random() -> PredefinedProfileImage {
+        return PredefinedProfileImage.allCases.randomElement()!
+    }
 
     var uiImage: UIImage? {
         UIImage(named: self.rawValue)
