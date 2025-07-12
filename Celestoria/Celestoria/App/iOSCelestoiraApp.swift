4ddba87549b02594550e7a85a3998e3565fa07c1
30 <1224tpdud@gmail.com>
2025-07-12 15:36:37 +0900

    ✨ Add iOS Target

---
+++ Celestoria/Celestoria/App/iOSCelestoiraApp.swift
@@ -0,0 +1,19 @@
+//
+//  iOSCelestoiraApp.swift
+//  Celestoria
+//
+//  Created by Park Seyoung on 7/12/25.
+//
+
+import SwiftUI
+
+@main
+struct iOSCelestoiraApp: App {
+    
+    var body: some Scene {
+        WindowGroup {
+            // 메인 뷰
+            iPhoneContentView()
+        }
+    }
+}
